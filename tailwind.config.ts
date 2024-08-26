1a281241f480eafcbf2a2822dec67f3f93947a0a
mumincelal <mumincelalpinar@gmail.com>
2024-08-26 15:36:19 +0300

    feat: update configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	darkMode: ["class"],
-	content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-		},
-	},
-	plugins: [require("tailwindcss-animate")],
+  darkMode: ["class"],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      }
+    }
+  },
+  plugins: [require("tailwindcss-animate")]
 };
 export default config;
